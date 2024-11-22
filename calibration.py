ROOT fit. Without correct logic for overflow

calibration.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import ROOT
 
 freq = 12e6
 channels = ['00', '01', '10', '11']
@@ -52,7 +53,6 @@ def plot_calib(dir, channels):
         plt.title(f'Channel {int(channel, 2)}')
         
     plt.savefig(f'./data/{timestamp}/calibration.pdf', dpi=300)
-    #plt.show()
     
     return all_data, all_bins, all_timestamps
 
@@ -85,16 +85,41 @@ def coincidence(timestamps, data, channels):
     mask = diff < 3
     diff = diff[mask]
     
-    plt.figure(figsize=(12, 6))
-    plt.hist(diff, bins = 15, edgecolor = 'black', linewidth = .5)
-    plt.xlabel('Time difference [ns]')
-    plt.ylabel('Entries')
-    plt.title('Time difference between channels with mean')
-    plt.legend([f'Variance = {np.var(diff):.4f} ns'])
-    plt.savefig(f'./data/{timestamp}/coincidence_mean.pdf', dpi=300)
-    plt.show()
+    plot_hist(diff*1e3)
     
+def plot_hist(data):
+    range_ = [int(np.min(data)//100 * 100) - 50 ,int((np.max(data)) + 100)//100 * 100 + 50]
+    length_ = int((range_[1] - range_[0])/100)
 
+    hist = ROOT.TH1F('Statistics', 'Coincidence with mean', length_ + 2, range_[0] - 100, range_[1] + 100)
+    
+    for i in data:
+        hist.Fill(i)
+    hist.Scale(1/hist.Integral("width"))
+    
+    q_ = np.zeros(2)
+    hist.GetQuantiles(2, q_, np.array([0.01, 0.99]))
+    q0 = q_[0]
+    q1 = q_[1]
+    
+    func = ROOT.TF1('func', 'ROOT::Math::normal_pdf(x, [0], [1])', q0, q1)   
+    func.SetParameters(hist.GetRMS(), hist.GetMean())
+    func.SetParNames('#sigma', '#mu')
+    hist.Fit('func', 'Q')
+    
+    fit_max = func.GetMaximum() * 1.1
+    
+    hist.GetYaxis().SetRangeUser(0, fit_max)
+    hist.GetXaxis().SetTitle('Time difference [ps]')
+    hist.GetYaxis().SetTitle('Abundance')
+    
+    ROOT.gStyle.SetOptFit(1)
+    ROOT.gStyle.SetOptStat(11)
+    
+    c = ROOT.TCanvas('c', 'c', 800, 600)
+    hist.Draw("hist")
+    func.Draw('same')
+    c.SaveAs(f'./data/{timestamp}/fit.pdf')
     
 
 data_, bins_, timestamps_ = plot_calib(timestamp, channels)